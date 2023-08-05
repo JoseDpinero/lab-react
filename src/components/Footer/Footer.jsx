091d098d6080b10215422dbae346feb45d9052c6
Jose Pinero <shine.shinete@gmail.com>
2023-08-05 14:43:57 +0200

    mise en place de routing + tailwind

---
+++ src/components/Footer/Footer.jsx
@@ -0,0 +1,5 @@
+import React from 'react'
+
+export default function Footer() {
+  return <div>Your Footer</div>
+}
---
+++ src/pages/Home/Home.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+export default function Home() {
+  return (
+    <div>
+      <h1>Bienvenue sur la page d'acceuil</h1>
+    </div>
+  )
+}
