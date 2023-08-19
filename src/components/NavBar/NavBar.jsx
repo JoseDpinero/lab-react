b8a77337919bbccf391922cbfc321086f4263b83
Jose Pinero <shine.shinete@gmail.com>
2023-08-19 15:53:57 +0200

    testing dropdown and active nav

--- src/components/NavBar/NavBar.jsx
+++ src/components/NavBar/NavBar.jsx
@@ -1,28 +1,34 @@
-import React from 'react'
+import React, { useState } from 'react'
 import { NavLink } from 'react-router-dom'
 
 export default function NavBar() {
+  const [show, setShow] = useState(false)
+
   return (
     <nav className="navBar">
-      <ul className="list-none flex gap-2">
-        <li>
-          <NavLink
-            className={({ isActive }) => (isActive ? 'nav-active nav' : 'nav')}
-            to="/"
-          >
-            Home
-          </NavLink>
-        </li>
-        <li></li>
-        <li>
-          <NavLink
-            className={({ isActive }) => (isActive ? 'nav-active nav' : 'nav')}
-            to="/contact"
-          >
-            Contact us
-          </NavLink>
-        </li>
-      </ul>
+      <button
+        className="text-yellow-800 uppercase gap-3 sm:hidden"
+        onClick={() => setShow(!show)}
+      >
+        Button
+      </button>
+      <div className="nav__container sm:flex">
+        <ul
+          className={`list-none flex gap-2 sm:flex ${show ? 'flex' : 'hidden'}`}
+        >
+          <li>
+            <NavLink className="nav" to="/">
+              Home
+            </NavLink>
+          </li>
+          <li></li>
+          <li>
+            <NavLink className="nav" to="/contact">
+              Contact us
+            </NavLink>
+          </li>
+        </ul>
+      </div>
     </nav>
   )
 }
